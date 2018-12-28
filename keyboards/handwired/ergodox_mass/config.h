049c399f661f8880eb1b03d3ed9d307b6bea357e
Andrew Mass <amass1212@gmail.com>
2018-12-28 08:19:04 -0600

    Fix bug with momentary layer switching.

--- keyboards/handwired/ergodox_mass/config.h
+++ keyboards/handwired/ergodox_mass/config.h
@@ -42,9 +42,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#define NO_DEBUG
 //#define NO_PRINT
 //#define NO_ACTION_LAYER
-#define NO_ACTION_TAPPING
+//#define NO_ACTION_TAPPING
 #define NO_ACTION_ONESHOT
-//#define NO_ACTION_MACRO
+#define NO_ACTION_MACRO
 #define NO_ACTION_FUNCTION
 
 // Mousekey Settings
